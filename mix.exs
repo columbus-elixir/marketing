7019d4675c144a67fe41e9e8afcc3bdc27e09504
Brian Crosby <croz1007@yahoo.com>
2018-02-07 14:29:19 -0500

    Added HAML

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule CbusElixir.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:materialize_sass, "~> 0.1.3"}
+      {:materialize_sass, "~> 0.1.3"},
+      {:phoenix_haml, "~> 0.2"}
     ]
   end
 
