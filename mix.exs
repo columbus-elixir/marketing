c17e7f9b41057c9c3a2ec80300710011e58b4b69
Brian Crosby <croz1007@yahoo.com>
2018-02-06 19:20:09 -0500

    Added sass and materialize

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule CbusElixir.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:sass, "~> 1.0"},
+      {:materialize_sass, "~> 0.1.3"}
     ]
   end
 
