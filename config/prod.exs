e553e6ca7515a32327efe0ff9052cfd323d67d5d
Brian Crosby <croz1007@yahoo.com>
2018-02-09 13:55:14 -0500

    Set up for Heroku Deploy

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,7 @@ config :cbus_elixir, CbusElixirWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :cbus_elixir, CbusElixirWeb.Repo,
+config :cbus_elixir, CbusElixir.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
