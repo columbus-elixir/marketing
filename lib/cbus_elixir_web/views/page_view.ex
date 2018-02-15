5a99d842597204c41fed8ff36033a3b4e9cc9a5a
Matt Darby <matt@matchedpattern.com>
2018-02-15 13:36:22 -0500

    Tweaking layout

--- lib/cbus_elixir_web/views/page_view.ex
+++ lib/cbus_elixir_web/views/page_view.ex
@@ -1,3 +1,19 @@
 defmodule CbusElixirWeb.PageView do
   use CbusElixirWeb, :view
+
+  def gravatar_url(email) do
+    hash =
+      email
+      |> String.trim
+      |> String.downcase
+      |> md5_hash
+      |> Base.encode16()
+      |> String.downcase
+
+    "https://www.gravatar.com/avatar/#{hash}"
+  end
+
+  defp md5_hash(email) do
+    :crypto.hash(:md5, email)
+  end
 end
