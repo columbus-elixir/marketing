972c0abd922241efb086cbcb1b8a3f863b232518
Matt Darby <matt@matchedpattern.com>
2018-02-20 11:57:02 -0500

    Adds next meeting display to UI

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule CbusElixir.Mixfile do
   def application do
     [
       mod: {CbusElixir.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :timex]
     ]
   end
 
@@ -40,7 +40,8 @@ defmodule CbusElixir.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:timex, "~> 3.1"}
     ]
   end
 
