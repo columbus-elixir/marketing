2415f5f0094876a42a05aae0deecedd876ae2b12
Matt Darby <matt@matchedpattern.com>
2018-02-21 12:26:41 -0500

    Meetings inserting correctly

--- priv/repo/migrations/20180221155850_create_meetings.exs
+++ priv/repo/migrations/20180221155850_create_meetings.exs
@@ -8,19 +8,4 @@ defmodule CbusElixir.Repo.Migrations.CreateMeetings do
     end
   end
 
-  def next_meeting(date \\ nil)
-  def next_meeting(date) when is_nil(date), do: next_meeting(Timex.now("America/Chicago"))
-  def next_meeting(date) do
-    # First Tuesday of each month
-    start = Timex.beginning_of_month(date)
-    days = Enum.find(0..6, fn d -> Timex.shift(start, days: d) |> Date.day_of_week() == 2 end)
-    Timex.shift(start, days: days)
-  end
-
-  start = Timex.now
-  Enum.with_index(0..100, fn(i) ->
-    date = Timex.shift(start, months: i)
-    CbusElixir.Repo.insert(CbusElixir.App.Meeting, %{date: next_meeting(date)})
-  end)
-
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,16 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+start = Timex.beginning_of_month(Timex.now)
+
+0..100
+|> Enum.with_index
+|> Enum.each(fn({x, i}) ->
+  date = Timex.shift(start, months: i)
+  days = Enum.find(0..6, fn d -> Timex.shift(date, days: d) |> Date.day_of_week() == 2 end)
+  meeting_date = Timex.shift(date, days: days)
+
+  CbusElixir.App.Meeting.changeset(%CbusElixir.App.Meeting{}, %{date: meeting_date})
+  |> CbusElixir.Repo.insert
+end)
