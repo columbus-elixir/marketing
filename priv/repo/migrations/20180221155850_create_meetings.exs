2ff17606625aab1235ea846acc8548a3c334549c
Brian Crosby <croz1007@yahoo.com>
2018-02-21 11:03:28 -0500

    Moved the next_meeting method to migration

--- priv/repo/migrations/20180221155850_create_meetings.exs
+++ priv/repo/migrations/20180221155850_create_meetings.exs
@@ -9,4 +9,19 @@ defmodule CbusElixir.Repo.Migrations.CreateMeetings do
     end
 
   end
+
+  def next_meeting(date \\ nil)
+  def next_meeting(date) when is_nil(date), do: next_meeting(Timex.now("America/Chicago"))
+  def next_meeting(date) do
+    # First Tuesday of each month
+    start = Timex.beginning_of_month(date)
+    days = Enum.find(0..6, fn d -> Timex.shift(start, days: d) |> Date.day_of_week() == 2 end)
+    meeting_date = Timex.shift(start, days: days)
+
+    case Timex.after?(date, meeting_date) do
+      false -> Timex.format!(meeting_date, "{YYYY}-{0M}-{0D}")
+      true -> next_meeting(Timex.shift(meeting_date, months: 1))
+    end
+  end
+
 end
