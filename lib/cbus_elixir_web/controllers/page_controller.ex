cdc4d4fcba3c48dba382c584bbef05e0690f02f1
Brian Crosby <croz1007@yahoo.com>
2018-02-23 12:39:51 -0500

    Moved functions from PageController to PageView

--- lib/cbus_elixir_web/controllers/page_controller.ex
+++ lib/cbus_elixir_web/controllers/page_controller.ex
@@ -1,21 +1,8 @@
 defmodule CbusElixirWeb.PageController do
-  import Ecto.Query, only: [from: 2]
   use CbusElixirWeb, :controller
 
-  alias CbusElixir.Repo
-  alias CbusElixir.App.Meeting
-
-  def index(conn, _params) do
-    today = Timex.today()
-    meeting = Repo.one!(from m in Meeting,
-      where: m.date > type(^today, Ecto.Date),
-      order_by: [asc: m.date],
-      preload: [:speakers],
-      limit: 1)
-    
-    date = Timex.to_date(meeting.date) |> Date.to_string
-    
-    render conn, "index.html", speakers: meeting.speakers, date: date
+  def index(conn, _params) do 
+    render conn, "index.html"
   end
 
 end
--- lib/cbus_elixir_web/views/page_view.ex
+++ lib/cbus_elixir_web/views/page_view.ex
@@ -1,7 +1,11 @@
 require IEx
 
 defmodule CbusElixirWeb.PageView do
+  import Ecto.Query, only: [from: 2]
   use CbusElixirWeb, :view
+
+  alias CbusElixir.Repo
+  alias CbusElixir.App.Meeting
 
   def gravatar_url(email) do
     hash =
@@ -13,6 +17,30 @@ defmodule CbusElixirWeb.PageView do
       |> String.downcase()
 
     "https://www.gravatar.com/avatar/#{hash}"
+  end
+
+  def get_next_meeting() do
+    today = Timex.today()
+    meeting = Repo.one!(from m in Meeting,
+      where: m.date > type(^today, Ecto.Date),
+      order_by: [asc: m.date],
+      limit: 1,
+      select: m.date)
+    meeting
+  end
+
+  def get_meeting_speakers() do
+    today = Timex.today()
+    meeting = Repo.one!(from m in Meeting,
+      where: m.date > type(^today, Ecto.Date),
+      order_by: [asc: m.date],
+      preload: [:speakers],
+      limit: 1)
+    meeting
+  end
+
+  def formated_date(date) do
+    Timex.to_date(date) |> Date.to_string
   end
 
   defp md5_hash(email) do
