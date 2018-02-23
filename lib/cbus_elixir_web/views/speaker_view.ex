832b273208fa3c26c398fe051afc603ef8e7c445
Brian Crosby <croz1007@yahoo.com>
2018-02-23 11:16:15 -0500

    Moved get_meetings method to view

--- lib/cbus_elixir_web/views/speaker_view.ex
+++ lib/cbus_elixir_web/views/speaker_view.ex
@@ -1,3 +1,24 @@
 defmodule CbusElixirWeb.SpeakerView do
+  import Ecto.Query
   use CbusElixirWeb, :view
+
+  alias CbusElixir.Repo
+  alias CbusElixir.App.Meeting
+
+  def get_meetings do
+    today = Timex.today()
+    # meets = CbusElixir.Repo.all(from m in "meetings", 
+    #   where: m.date > type(^today, Ecto.Date),
+    #   limit: 5,
+    #   select: {m.id, m.date})
+
+    meets = Columbus.Repo.all(from m in Meeting,
+      where: m.date > type(^today, Ecto.Date),
+      order_by: [asc: m.id],
+      limit: 5,
+      select: {m.id, m.date}
+    )
+    meetings = for {k, v} <- meets, do: {Timex.to_date(v) |> Date.to_string, k}
+  end
+
 end
