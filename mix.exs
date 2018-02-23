be2411bf129eb994ee8c18cac43fa382b7b520d7
Brian Crosby <croz1007@yahoo.com>
2018-02-23 11:16:25 -0500

    Updated deps

--- mix.exs
+++ mix.exs
@@ -35,13 +35,13 @@ defmodule CbusElixir.Mixfile do
     [
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
+      {:phoenix_ecto, "~> 3.3"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:timex, "~> 3.1"},
+      {:timex, "~> 3.2"},
       {:timex_ecto, "~> 3.2"}
     ]
   end
