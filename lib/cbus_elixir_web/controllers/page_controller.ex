22e36458bf83ed895701cc984dec75349c755af1
Brian Crosby <croz1007@yahoo.com>
2018-03-01 16:16:37 -0500

    Added query string handling with tests

--- lib/cbus_elixir_web/controllers/page_controller.ex
+++ lib/cbus_elixir_web/controllers/page_controller.ex
@@ -3,10 +3,20 @@ defmodule CbusElixirWeb.PageController do
 
   alias CbusElixir.App.Meetings
 
-  def index(conn, _params) do
-    next_meeting = Meetings.next_meeting(Timex.today())
-
+  def index(conn, params) do
+    next_meeting = Meetings.next_meeting(parse_meeting_date(params["date"]))
     render conn, "index.html", next_meeting: next_meeting
+  end
+
+  defp parse_meeting_date(nil), do: Timex.today()
+
+  defp parse_meeting_date(date) do
+    date
+    |> Date.from_iso8601() 
+    |> case do
+      {:ok, result} -> result
+      {:error, _ } -> Timex.today()
+    end
   end
 
 end
