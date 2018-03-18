bdcfea1623b2096b3d6b8db60c79d292896c51a9
Robb <kimchibot64@gmail.com>
2018-03-18 12:36:42 -0400

    refactor of seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,46 +1,58 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     CbusElixir.Repo.insert!(%CbusElixir.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule Seeder do
+  # Script for populating the database. You can run it as:
+  #
+  #     mix run priv/repo/seeds.exs
+  #
+  # Inside the script, you can read and write to any of your
+  # repositories directly:
+  #
+  #     CbusElixir.Repo.insert!(%CbusElixir.SomeSchema{})
+  #
+  # We recommend using the bang functions (`insert!`, `update!`
+  # and so on) as they will fail if something goes wrong.
 
-alias CbusElixir.Repo
-alias CbusElixir.App.Meeting
-alias CbusElixir.App.Speaker
-
-start = Timex.beginning_of_month(Timex.now)
-
-
-Enum.each(0..10, fn(x) ->
-  date = Timex.shift(start, months: x)
-  days = Enum.find(0..6, fn d -> Timex.shift(date, days: d) |> Date.day_of_week() == 2 end)
-  meeting_date = Timex.shift(date, days: days)
-  speaker = %{ email: Faker.Internet.email, name: Faker.Name.name, title: Faker.Name.title, url: Faker.Internet.url }
+  alias CbusElixir.Repo
+  alias CbusElixir.App.Meeting
+  alias CbusElixir.App.Speaker
 
 
-  changeset = %Meeting{}
-    |> Meeting.changeset(%{date: meeting_date})
+  def run_seeds do
+    start = Timex.beginning_of_month(Timex.now)
 
-    case Repo.insert(changeset) do
-      {:ok, meeting } ->
-        changeset = meeting
-          |> Ecto.build_assoc(:speakers)
-          |> Speaker.changeset(speaker)
+    Enum.each(0..10, fn(x) ->
+      date = Timex.shift(start, months: x)
+      days = Enum.find(0..6, fn d -> Timex.shift(date, days: d) |> Date.day_of_week() == 2 end)
+      meeting_date = Timex.shift(date, days: days)
+      add_meeting(meeting_date)
+      end)
+  end
 
-          case Repo.insert(changeset) do
-            {:ok, speaker } ->
-              IO.inspect("Speaker was added")
-            {:error, _reason} ->
-              IO.puts("Speaker was not added")
-          end
-      {:error, _reason} ->
-        IO.puts("Meeting was added")
-    end
+  def add_meeting(meeting_date) do
+    changeset = %Meeting{}
+      |> Meeting.changeset(%{date: meeting_date})
 
-end)
+      case Repo.insert(changeset) do
+        {:ok, meeting } ->
+          IO.puts("Meeting was added")
+          add_speaker(meeting)
+        {:error, _reason} ->
+          IO.puts("Meeting was not added")
+      end
+  end
+
+  def add_speaker(meeting) do
+    changeset = meeting
+      |> Ecto.build_assoc(:speakers)
+      |> Speaker.changeset(%{email: Faker.Internet.email, name: Faker.Name.name, title: Faker.Name.title, url: Faker.Internet.url})
+
+      case Repo.insert(changeset) do
+        {:ok, speaker } ->
+          IO.inspect("Speaker was added")
+        {:error, _reason} ->
+          IO.puts("Speaker was not added")
+      end
+  end
+
+end
+
+Seeder.run_seeds()
