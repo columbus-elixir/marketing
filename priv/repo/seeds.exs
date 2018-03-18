c19768c074608bbc23dae036d6b5cebdd446eed6
Robb <kimchibot64@gmail.com>
2018-03-18 09:33:59 -0400

    added speakers to seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,16 +12,35 @@
 
 alias CbusElixir.Repo
 alias CbusElixir.App.Meeting
+alias CbusElixir.App.Speaker
 
 start = Timex.beginning_of_month(Timex.now)
 
 
-Enum.each(0..100, fn(x) ->
+Enum.each(0..10, fn(x) ->
   date = Timex.shift(start, months: x)
   days = Enum.find(0..6, fn d -> Timex.shift(date, days: d) |> Date.day_of_week() == 2 end)
   meeting_date = Timex.shift(date, days: days)
+  speaker = %{ email: Faker.Internet.email, name: Faker.Name.name, title: Faker.Name.title, url: Faker.Internet.url }
 
-  %Meeting{}
-  |> Meeting.changeset(%{date: meeting_date})
-  |> Repo.insert!
+
+  changeset = %Meeting{}
+    |> Meeting.changeset(%{date: meeting_date})
+
+    case Repo.insert(changeset) do
+      {:ok, meeting } ->
+        changeset = meeting
+          |> Ecto.build_assoc(:speakers)
+          |> Speaker.changeset(speaker)
+
+          case Repo.insert(changeset) do
+            {:ok, speaker } ->
+              IO.inspect("Speaker was added")
+            {:error, _reason} ->
+              IO.puts("Speaker was not added")
+          end
+      {:error, _reason} ->
+        IO.puts("Meeting was added")
+    end
+
 end)
