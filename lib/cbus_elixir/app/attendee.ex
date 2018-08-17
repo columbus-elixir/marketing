7db95e417acc6fd418c6f9c38080c2ccf6fe053b
Robb <kimchibot64@gmail.com>
2018-08-17 16:55:53 -0400

    added twitter handle formatter

--- lib/cbus_elixir/app/attendee.ex
+++ lib/cbus_elixir/app/attendee.ex
@@ -20,5 +20,22 @@ defmodule CbusElixir.App.Attendee do
     attendee
     |> cast(attrs, [:name, :email, :twitter, :new_to_elixir, :new_to_cbus_elixir, :meeting_id])
     |> validate_required([:name, :email, :new_to_elixir, :new_to_cbus_elixir, :meeting_id])
+    |> validate_length(:twitter, min: 1, max: 15)
+    |> validate_format(:email, ~r/@/)
+    |> format_twitter
   end
+
+  defp format_twitter(%{valid?: false} = changeset), do: changeset
+
+  defp format_twitter(changeset) do
+    twitter = get_change(changeset, :twitter)
+    invalid_handle_input = Regex.scan(~r/[^\w+?\@]/, twitter)
+    
+    if length(invalid_handle_input) > 0 do
+      Ecto.Changeset.add_error(changeset, :twitter, "Characters #{invalid_handle_input} not allowed in Twitter handle")
+    else
+      put_change(changeset, :twitter, Regex.replace(~r/\W+/, twitter, ""))
+    end
+  end
+
 end
