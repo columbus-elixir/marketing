c1966e6b910a7d0e7ef6a2566c9a43004204f1e1
Robb <kimchibot64@gmail.com>
2018-08-17 17:28:46 -0400

    added meeting registration controller tests

---
+++ test/cbus_elixir_web/controllers/meeting_registration_controller_test.exs
@@ -0,0 +1,21 @@
+defmodule CbusElixirWeb.MeetingRegistrationControllerTest do
+    use CbusElixirWeb.ConnCase
+  
+    test "GET /", %{conn: conn} do
+    conn = get conn, meeting_registration_path(conn, :index)
+      assert html_response(conn, 200) =~ "Registration for"
+    end
+  
+    test "Shows correct meeting day of", %{conn: conn} do
+      date = "2018-03-25"
+      conn = get conn, "/?date=#{date}"
+      assert html_response(conn, 200) =~ "2018-04-03"
+    end
+
+    test "Shows correct meeting day of meeting", %{conn: conn} do
+        date = "2018-04-03"
+        conn = get conn, "/?date=#{date}"
+        assert html_response(conn, 200) =~ "2018-04-03"
+      end
+  
+  end
