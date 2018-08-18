fdba2e170be5e3145327880bfd4c578c129be5f5
Robb <kimchibot64@gmail.com>
2018-08-18 18:59:41 -0400

    fixed some tests

--- test/cbus_elixir_web/controllers/attendee_controller_test.exs
+++ test/cbus_elixir_web/controllers/attendee_controller_test.exs
@@ -2,6 +2,8 @@ defmodule CbusElixirWeb.AttendeeControllerTest do
   use CbusElixirWeb.ConnCase
 
   @create_attrs %{email: "some@email", name: "some name", new_to_cbus_elixir: true, new_to_elixir: true, twitter: "@handle", meeting_id: 1}
+  @create_attrs2 %{email: "some@email", name: "some name", new_to_cbus_elixir: true, new_to_elixir: true, meeting_id: 1}
+
   @invalid_attrs %{email: nil, name: nil, new_to_cbus_elixir: nil, new_to_elixir: nil, twitter: "@handle", meeting_id: nil}
   @invalid_twitter_attrs1 %{email: "some@email", name: "some name", new_to_cbus_elixir: true, new_to_elixir: true, twitter: "@@h$a%ndle", meeting_id: 1}
   @invalid_twitter_attrs2 %{email: "some@email", name: "some name", new_to_cbus_elixir: true, new_to_elixir: true, twitter: "thishastoomanycharacters", meeting_id: 1}
@@ -20,7 +22,16 @@ defmodule CbusElixirWeb.AttendeeControllerTest do
       assert redirected_to(conn) == meeting_registration_path(conn, :index)
 
       conn = get conn, meeting_registration_path(conn, :index)
-      assert html_response(conn, 200) =~ "Register for this meetup"
+      assert html_response(conn, 200) =~ "Attendee created successfully."
+    end
+
+    test "redirects to meeting registration when no twitter handle", %{conn: conn} do
+      conn = post conn, attendee_path(conn, :create), attendee: @create_attrs2
+
+      assert redirected_to(conn) == meeting_registration_path(conn, :index)
+
+      conn = get conn, meeting_registration_path(conn, :index)
+      assert html_response(conn, 200) =~ "Attendee created successfully."
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
