32ee440157ba194c8343a693d229edadc4ed3554
Codball <NathanCCaldwell@gmail.com>
2019-01-19 22:29:17 -0500

    test data seed for meetings

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,6 +12,7 @@
 
 alias CbusElixir.Repo
 alias CbusElixir.App.Meeting
+alias CbusElixir.App.Speaker
 
 start_date = ~N[2018-03-01T00:00:00]
 
@@ -24,3 +25,131 @@ Enum.each(0..100, fn x ->
   |> Meeting.changeset(%{date: meeting_date})
   |> Repo.insert!()
 end)
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 1})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 2})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 3})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 4})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 5})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 6})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 7})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 8})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 9})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 10})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 11})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 12})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 13})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 14})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 15})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 16})
+  |> Repo.insert!()
+  
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 17})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 18})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 19})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 20})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 21})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 22})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 23})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 24})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 25})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 26})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 27})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 28})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 29})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 30})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 31})
+  |> Repo.insert!()
+
+%Speaker{}
+  |> Speaker.changeset(%{email: "test@test.com", name: "test", title: "test", url: "test.com", meeting_id: 32})
+  |> Repo.insert!()
