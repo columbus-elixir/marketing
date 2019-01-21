149ba08fdb82d7a23bef42b1f4172fa76e8072c6
Codball <NathanCCaldwell@gmail.com>
2019-01-21 17:11:57 -0500

    typespec fix

--- lib/cbus_elixir/app/meetings.ex
+++ lib/cbus_elixir/app/meetings.ex
@@ -5,6 +5,7 @@ defmodule CbusElixir.App.Meetings do
 
   import Ecto.Query
 
+  alias Ecto
   alias CbusElixir.Repo
   alias CbusElixir.App.Meeting
   alias CbusElixir.Pagination
@@ -16,7 +17,7 @@ defmodule CbusElixir.App.Meetings do
   """
   def meetings_for_page(page \\ 1, per_page \\ 5)
 
-  @spec meetings_for_page(integer(), integer()) :: [Ecto.schema.t()]
+  @spec meetings_for_page(integer(), integer()) :: map()
   def meetings_for_page(page, per_page) do
     query =
       from(m in Meeting,
@@ -33,7 +34,7 @@ defmodule CbusElixir.App.Meetings do
   Returns the attendees for the given meeting
   """
 
-  @spec attendees_for_meeting(integer()) :: [Ecto.schema.t()]
+  @spec attendees_for_meeting(integer()) :: map()
   def attendees_for_meeting(id) do
     query = 
       from(a in Attendee,
@@ -50,7 +51,7 @@ defmodule CbusElixir.App.Meetings do
   Returns the next `count` upcoming meetings
   """
 
-  @spec upcoming_meetings(integer(), integer()) :: [Ecto.schema.t()]
+  @spec upcoming_meetings(integer(), integer()) :: map()
   def upcoming_meetings(date, count: count) do
     Meeting
     |> Meeting.happening_after(date)
@@ -62,7 +63,7 @@ defmodule CbusElixir.App.Meetings do
   Returns the next upcoming meeting including speakers
   """
 
-  @spec next_meeting(integer()) :: [Ecto.schema.t()]
+  @spec next_meeting(integer()) :: map()
   def next_meeting(date) do
     Meeting
     |> Meeting.happening_after(date)
