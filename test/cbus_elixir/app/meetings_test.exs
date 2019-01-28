3517a47d3e5a1d5decbadf30b7456101e4e49a27
Codball <NathanCCaldwell@gmail.com>
2019-01-28 13:05:43 -0500

    Testing for meetings_for_page_query/0

---
+++ test/cbus_elixir/app/meetings_test.exs
@@ -0,0 +1,30 @@
+defmodule CbusElixir.MeetingsTest do
+  use CbusElixir.DataCase
+
+  alias CbusElixir.Repo
+  alias CbusElixir.App.Meeting
+  alias CbusElixir.App.Meetings
+
+  def create_meeting_for_week(week) when is_integer(week) do
+    meeting_date = Timex.shift(~N[2018-01-01T00:00:00], days: week * 7)
+
+    %Meeting{}
+    |> Meeting.changeset(%{date: meeting_date})
+    |> Repo.insert!()
+  end
+
+  describe "meetings_for_page_query/0" do
+    test "it returns meetings in the correct order" do
+      oldest = create_meeting_for_week(1)
+      middle = create_meeting_for_week(2)
+      newest = create_meeting_for_week(3)
+
+      result = Meetings.meetings_for_page_query() |> Repo.all()
+
+      assert length(result) == 3
+      assert :lists.nth(1, result).id == newest.id
+      assert :lists.nth(2, result).id == middle.id
+      assert :lists.nth(3, result).id == oldest.id
+    end
+  end
+end
