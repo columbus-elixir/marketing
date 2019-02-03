e93248b48455e4b69d84b18f9716b6faa8f42b27
Codball <NathanCCaldwell@gmail.com>
2019-02-03 15:34:35 -0500

    removed comment

--- lib/cbus_elixir_web/controllers/meeting_controller.ex
+++ lib/cbus_elixir_web/controllers/meeting_controller.ex
@@ -11,15 +11,6 @@ defmodule CbusElixirWeb.MeetingController do
     per_page = params["per_page"] || 5
     paged_result = Pagination.paginate(Meetings.meetings_for_page_query(), page, per_page)
 
-    """
-    It looks like the limit is being applied to the rows + the joined rows (speakers).
-    We should check out [scrivener_ecto](https://github.com/drewolson/scrivener_ecto) and see
-    if that fixes this issue. As a temporary hack, we can call Repo.preload/2 on the results
-    after they have been paginated. This causes an additional query to execute in order to
-    fill the associated Speaker rows. This SO looks like it covers the issue:
-    (https://stackoverflow.com/questions/21832104/left-outer-join-with-limit)
-    """
-
     preloaded_results =
       Map.get(paged_result, :results)
       |> Repo.preload(:speakers)
