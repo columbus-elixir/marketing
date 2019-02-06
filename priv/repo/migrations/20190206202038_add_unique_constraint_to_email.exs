4f8d65b2e3ce86ba1639ad47c0b9d2a2c9f40492
Brian Crosby <croz1007@gmail.com>
2019-02-06 17:42:18 -0500

    Formatted

--- priv/repo/migrations/20190206202038_add_unique_constraint_to_email.exs
+++ priv/repo/migrations/20190206202038_add_unique_constraint_to_email.exs
@@ -2,6 +2,6 @@ defmodule CbusElixir.Repo.Migrations.AddUniqueConstraintToEmail do
   use Ecto.Migration
 
   def change do
-    create unique_index(:attendees, [:email])
+    create(unique_index(:attendees, [:email]))
   end
 end
