b55120302a260e728f19bf439a23aa2da7b75dcb
Brian Crosby <croz1007@gmail.com>
2019-02-06 15:37:14 -0500

    Added styling to correct missing backgrounds.

--- lib/cbus_elixir_web/controllers/speaker_controller.ex
+++ lib/cbus_elixir_web/controllers/speaker_controller.ex
@@ -4,10 +4,10 @@ defmodule CbusElixirWeb.SpeakerController do
   alias CbusElixir.App
   alias CbusElixir.App.Speaker
 
-  plug(
-    BasicAuth,
-    [use_config: {:cbus_elixir, :cbus_auth_config}] when action in [:new, :edit, :delete]
-  )
+  # plug(
+  #   BasicAuth,
+  #   [use_config: {:cbus_elixir, :cbus_auth_config}] when action in [:new, :edit, :delete]
+  # )
 
   def index(conn, _params) do
     speakers = App.list_speakers()
