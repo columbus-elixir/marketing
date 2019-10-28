e9c21bbb88005d248266571b17f11b6a5e3daa68
Izzy Elwyn <32496537+IzzyElwyn@users.noreply.github.com>
2019-10-28 18:26:12 -0400

    Revert "Add link to RSVP to meeting events"

--- priv/repo/migrations/20191028151617_add_rsvp_link_to_meetings.exs
+++
@@ -1,9 +0,0 @@
-defmodule CbusElixir.Repo.Migrations.AddRsvpLinkToMeetings do
-  use Ecto.Migration
-
-  def change do
-    alter table(:meetings) do
-      add :rsvp_link, :string
-    end
-  end
-end
