0dbaff13be5ca6c8ab6e85b06dc4e2ba5a28abe1
Brian Crosby <croz1007@gmail.com>
2019-10-28 18:14:45 -0400

    Added migration to correct unique index

---
+++ priv/repo/migrations/20191028220401_correct-unique-index-on-attendees.exs
@@ -0,0 +1,8 @@
+defmodule :"Elixir.CbusElixir.Repo.Migrations.Correct-unique-index-on-attendees" do
+  use Ecto.Migration
+
+  def change do
+    remove(unique_index(:attendees, [:email]))
+    create(unique_index(:attendees, [:email, :meeting_id]))
+  end
+end
